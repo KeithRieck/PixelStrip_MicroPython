0cc6103ecb52d275b9bcea4a6d2c17f7035e9305
Keith Rieck <KeithRieck@gmail.com>
2021-08-25 20:53:44 -0500

    npxl name change

--- npxl.py
+++ npxl.py
@@ -4,7 +4,7 @@ from machine import Pin
 import rp2
 
 @rp2.asm_pio(sideset_init=rp2.PIO.OUT_LOW, out_shiftdir=rp2.PIO.SHIFT_LEFT, autopull=True, pull_thresh=24)
-def ws2812():
+def __pio_for_ws2812():
     T1 = 2
     T2 = 5
     T3 = 3
@@ -19,18 +19,42 @@ def ws2812():
 
 
 class NeoPixel:
-    def __init__(self, pin_num, num_pixels, brightness=0.5):
+    def __init__(self, pin_num, num_pixels, bpp=3, brightness=1.0, auto_write=True, pixel_order=None):
+        self.pin = Pin(pin_num)
         self._pin_num = pin_num
         self._num_pixels = num_pixels
+        self._bpp = bpp
         self._brightness = brightness
+        self.auto_write = auto_write
+        self._pixel_order = pixel_order
         self._ar = array.array("I", [0 for _ in range(num_pixels)])
-        self._sm = rp2.StateMachine(0, ws2812, freq=8_000_000, sideset_base=Pin(pin_num))
+        self._sm = rp2.StateMachine(0, __pio_for_ws2812, freq=8_000_000, sideset_base=self.pin)
         self._sm.active(1)
     
-    def __setitem__(self, index, color):
-        self._ar[index] = (color[1]<<16) + (color[0]<<8) + color[2]
+    def deinit(self):
+        self.fill((0, 0, 0, 0))
+        self.show()
+        # self.pin.deinit()
 
-    def __getitem__(self, c):
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.deinit()
+
+    def __repr__(self):
+        return "[" + ", ".join([str(x) for x in self]) + "]"
+
+    def _set_item(self, index, r, g, b, w): 
+        self._ar[index] = (g<<16) + (r<<8) + b
+
+    def __setitem__(self, index, color):
+        self._set_item(index, color[0], color[1], color[2], color[3])
+        if self.auto_write:
+            self.show()
+
+    def __getitem__(self, index):
+        c = self._ar[index]
         return (((c >> 8) & 0xFF), ((c>>16) & 0xFF), (c & 0xFF), 0)
     
     def __len__(self):
@@ -47,21 +71,32 @@ class NeoPixel:
         time.sleep_ms(10)
     
     def fill(self, color):
-        for p in range(self._num_pixels):
-            self[p] = color
+        for index in range(self._num_pixels):
+            self._set_item(index, color[0], color[1], color[2], color[3])
+        if self.auto_write:
+            self.show()
 
+    @property
+    def n(self):
+        return len(self)
+
+    @property
+    def bpp(self):
+        return self._bpp
+
+    @property
+    def brightness(self):
+        return self._brightness
+
+    @brightness.setter
+    def brightness(self, value):
+        self._brightness = min(max(value, 0.0), 1.0)
+        if self.auto_write:
+            self.show()
 
 # TODO:
-# rename ws2812 to __pio_for_ws2812
-# auto_write in constructor
-# brightness in constructor
-# NOOP constructor arguments for bpp and pixel_order
-# __repr__
-# deinit and __enter__ and __exit__
-# n as a property for strip length
-#
 # how to use libraries and multiple files on RPi?
-# Add and testPixelStrip stuff
+# Add and test PixelStrip module
 #
 # I2C as a peripheral device
 # Test with two strips
